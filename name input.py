1ee12e2f0325424af4dda35d9e02a24b9faee52f
NguyenThanhCong170 <142860612+NguyenThanhCong170@users.noreply.github.com>
2023-08-24 11:42:54 +0700

    Add files via upload

---
+++ name input.py
@@ -0,0 +1,9 @@
+name = input('ten cua ban la ')
+so_luong =  name.__len__()
+if so_luong < 3:
+    print('can phai co it nhat 3 chu cai')
+elif so_luong > 16:
+
+    print('so luong chu cai toi da la 16')
+else:
+    print('ten that la dep')
